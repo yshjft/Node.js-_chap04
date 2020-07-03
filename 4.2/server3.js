05f4514d7b7632121fa55bf191db3510eb31a011
hys <jericoh@naver.com>
2020-07-03 20:49:41 +0900

    chap 4.2

---
+++ 4.2/server3.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const parseCookies = (cookie = '')=>
+  cookie
+    .split(';')
+    .map(v => v.split('='))
+    .reduce((acc, [k,v]) => {
+      acc[k.trim()] = decodeURIComponent(v);
+      return acc;
+    }, {});
+
+http.createServer((req, res)=>{
+  const cookies=parseCookies(req.headers.cookie);
+  console.log(req.url, cookies);
+  res.writeHead(200, {'Set-Cookie' : 'mycookie=test'});
+  res.end('Hello Cookies');
+}).listen(8002, ()=>{
+  console.log('8002번 포트에서 서버 대기중입니다.');
+});
